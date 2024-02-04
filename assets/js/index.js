3749490b122d33b62049dcd29f7c517c6c4924e2
Shamima Haque <haqueshamima34@gmail.com>
2024-02-04 14:09:26 +0600

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+
+    setInterval(function (){
+        const d = new Date();
+        document.getElementById("time").innerHTML = d;
+    }, 1000)
+
+
+    $(document).ready(function() {
+        var owl = $('.owl-carousel');
+        owl.owlCarousel({
+            items: 4,
+            loop: true,
+            margin: 10,
+            autoplay: true,
+            autoplayTimeout: 1000,
+            autoplayHoverPause: true
+        });
+
+    })
